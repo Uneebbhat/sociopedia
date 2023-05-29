4795f95a1cf0d04d1efec30b63fd996106ef8862
Uneeb <uneebbhatti3@gmail.com>
2023-05-29 15:15:05 +0500

    currently working on signup page

---
+++ src/Components/Pages/SignUp/SignUp.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./SignUp.css";
+
+const SignUp = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="welcome__heading">
+          <p>Welcome to Sociopedia, the Social Media For Sociopathsics</p>
+        </div>
+        <div className="form">
+          <form action="/" className="form">
+            <div className="name__form">
+              <input
+                type="text"
+                className="name first__name"
+                placeholder="First Name"
+              />
+              <input type="text" className="name" placeholder="Last Name" />
+            </div>
+            <div className="email__form">
+              <input
+                type="email"
+                className="email"
+                placeholder="name@gmail.com"
+              />
+              <input
+                type="password"
+                className="password"
+                placeholder="password"
+              />
+            </div>
+            <div className="profile__pic">
+              <p>Add Picture Here</p>
+            </div>
+            <div className="button">
+              <button className="btn">Register</button>
+            </div>
+            <div className="sign__up">
+              <p>Already have an account? Login here</p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
