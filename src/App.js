814fec9c992bcca9863bf01a3e5b1b7e37135853
Uneeb <uneebbhatti3@gmail.com>
2023-06-03 15:05:00 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Components/Navbar/SimpleNavbar/Navbar";
-import SignUp from "./Components/Pages/SignUp/SignUp";
-import HomePage from "./Components/Pages/Home/HomePage";
-import HomeNav from "./Components/Navbar/HomeNavbar/HomeNav";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/signup"
-          element={
-            <>
-              <Navbar />
-              <SignUp />
-            </>
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <>
-              <HomeNav />
-              <HomePage />
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+    <>
+      <h1>hello world!</h1>
+    </>
   );
 }
 
